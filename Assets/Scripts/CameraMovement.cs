026ff65b55220b665adf2e77eef898a41fc13995
Duzz <osmansisman433@gmail.com>
2023-10-06 14:17:59 +0300

    zort

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,16 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+
 
 public class CameraMovement : MonoBehaviour
 {
-    void Start()
+    private NewControls cameraActions;
+    private InputAction movement;
+    private Transform cameraTransform;
+
+    [Header("Horizontal Translation")]
+    
+    [SerializeField]
+    private float maxSpeed=5f;
+    private float speed;
+    private float acceleration=10f;
+    private float damping=15f;
+
+    [Header("Vertical Translation")]
+    [SerializeField]
+    private float stepSize=2f;
+    private float zoomDampening=7.5f;
+    private float maxHeight=10f;
+    private float minHeight=10f;
+    private float zoomSpeed=2f;
+    
+   
+
+    
+    
+
+
+
+
+
+
+
+
+    public void OnMove()
     {
-        
-    }
- 
-    void Update()
-    {
-        
+
     }
 }
